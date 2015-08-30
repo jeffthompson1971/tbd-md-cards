6efbace4c8e91a0a9c619e3d8f3b622644cede83
Jeff Thompson <jthomps1@motorola.com>
2015-08-30 14:58:06 -0500

    added templates and style

--- directive.js
+++ directive.js
@@ -27,4 +27,34 @@ angular.module('jeffthompson1971.md-cards', []).
         },
     }
     
-  });
+  })
+
+  .directive('mdCardImageOverlay', function () {
+
+    return {
+
+        scope: {
+            listing: '='
+
+        },
+
+        templateUrl: "templates/md-card-image-overlay.html",
+
+        link: function (scope, element, attrs) {
+
+            scope.title = attrs.title;
+
+            // watch for changes in the listing to update the new photo
+            scope.$watch('listing', function (listing) {
+
+                var el = angular.element(element.find('#md-card-image'));
+
+                el.css({
+                    'background-image': 'url(' + listing.photoUrl + ')'
+
+                });
+            });
+        }
+    }
+)
+
